fff0bed8e1cdfea63e290a4b166bbde87d912df9
Chris Thomas <faore@onhaven.net>
2016-10-21 08:54:37 -0600

    Additional Predicates, Explaination of Broken Agent.

--- src/csci446/project2/Main.java
+++ src/csci446/project2/Main.java
@@ -15,5 +15,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 	    TestRunner runner = new TestRunner(10, .05, .05, .05);
+        System.out.println("We were unable to successfully create a knowledge agent using first-order logic.\n" +
+                "We had trouble with resolution and unification in the inference engine.\n" +
+                "While, the knowledge agent still runs, it always returns null for a move.\n" +
+                "The knowledge base is still created and does store starting facts about the world, as well as a ruleset." +
+                "They are just not evaluated and updated by the inference engine and no best actions are generated based" +
+                "on it.");
     }
 }
