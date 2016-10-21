af09d536789dcdab1dd9822be1736a615edc0583
Chris Thomas <faore@onhaven.net>
2016-10-21 09:34:21 -0600

    Add Negation Rules

--- src/csci446/project2/Agents/Inference/Rules.java
+++ src/csci446/project2/Agents/Inference/Rules.java
@@ -8,18 +8,79 @@ import java.util.ArrayList;
 public class Rules {
     public static ArrayList<Sentence> generateRules() throws Exception {
         ArrayList<Sentence> rules = new ArrayList<Sentence>();
+
+        //Negation Rules
+        //Safe/Unsafe
+        Variable x = new Variable();
+        Clause implicant = new Clause(Predicate.Safe, x);
+        Clause negation = new Clause(Predicate.Unsafe, x, true);
+        ArrayList<Clause> clauses = new ArrayList<Clause>();
+        ArrayList<Operator> operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+        //Wumpus/NotWumpus
+        x = new Variable();
+        implicant = new Clause(Predicate.Wumpus, x);
+        negation = new Clause(Predicate.NotWumpus, x, true);
+        clauses = new ArrayList<Clause>();
+        operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+
+        //Pit/NotPit
+        x = new Variable();
+        implicant = new Clause(Predicate.Pit, x);
+        negation = new Clause(Predicate.NotPit, x, true);
+        clauses = new ArrayList<Clause>();
+        operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+
+        //Obstacle/NotObstacle
+        x = new Variable();
+        implicant = new Clause(Predicate.Obstacle, x);
+        negation = new Clause(Predicate.NotObstacle, x, true);
+        clauses = new ArrayList<Clause>();
+        operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+
+        //Stench/NotStench
+        x = new Variable();
+        implicant = new Clause(Predicate.Stench, x);
+        negation = new Clause(Predicate.NotStench, x, true);
+        clauses = new ArrayList<Clause>();
+        operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+
+        //Breeze/NotBreeze
+        x = new Variable();
+        implicant = new Clause(Predicate.Breeze, x);
+        negation = new Clause(Predicate.NotBreeze, x, true);
+        clauses = new ArrayList<Clause>();
+        operators = new ArrayList<Operator>();
+        clauses.add(negation);
+
+        rules.add(new Sentence(Quantifier.ForAll, x, implicant, clauses, operators));
+
         //Create rules here. Order can matter. They are checked in the exact order declared.
         Variable spot = new Variable();
 
-        Clause implicant = new Clause(Predicate.Safe, spot, false);
-        Clause noWumpus = new Clause(Predicate.NotWumpus, spot, false);
-        Clause noPit = new Clause(Predicate.NotPit, spot, false);
-        ArrayList<Operator> operators = new ArrayList<Operator>();
-        ArrayList<Clause> clauses = new ArrayList<Clause>();
+        implicant = new Clause(Predicate.Safe, spot, false);
+        Clause c1 = new Clause(Predicate.NotWumpus, spot, false);
+        Clause c2 = new Clause(Predicate.NotPit, spot, false);
+        operators = new ArrayList<Operator>();
+        clauses = new ArrayList<Clause>();
 
-        clauses.add(noWumpus);
+        clauses.add(c1);
         operators.add(Operator.AND);
-        clauses.add(noPit);
+        clauses.add(c2);
         // Resulting Sentence:
         // ∀a Safe(a) <= NotWumpus(a) AND NotPit(a)
         Sentence sentence = new Sentence(Quantifier.ForAll, spot, implicant, clauses, operators);
@@ -32,14 +93,14 @@ public class Rules {
         operators.clear();
         
         implicant = new Clause(Predicate.Safe, spot, false);
-        noWumpus = new Clause(Predicate.NotStench, spot, false);
-        noPit = new Clause(Predicate.NotBreeze, spot, false);
+        c1 = new Clause(Predicate.NotStench, spot, false);
+        c2 = new Clause(Predicate.NotBreeze, spot, false);
         operators = new ArrayList<Operator>();
         clauses = new ArrayList<Clause>();
 
-        clauses.add(noWumpus);
+        clauses.add(c1);
         operators.add(Operator.AND);
-        clauses.add(noPit);
+        clauses.add(c2);
         // Resulting Sentence:
         // ∀a Safe(a) <= NotWumpus(a) AND NotPit(a)
         sentence = new Sentence(Quantifier.ForAll, spot, implicant, clauses, operators);
