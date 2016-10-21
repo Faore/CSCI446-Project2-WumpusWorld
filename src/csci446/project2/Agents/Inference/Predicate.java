19075e8747d270731646d1266db6d30aed2d32c7
Chris Thomas <faore@onhaven.net>
2016-10-20 20:08:18 -0600

    Predicates can now store be based on up to 2 variables.

--- src/csci446/project2/Agents/Inference/Predicate.java
+++ src/csci446/project2/Agents/Inference/Predicate.java
@@ -13,7 +13,10 @@ public class Predicate {
     //Predicates are relative to cells.
     public final Predicates predicateType;
     public final boolean negated;
-    public final Variable variable;
+    public final Variable variable1;
+    public final Variable variable2;
+
+    public final int variableCount;
 
     /*
     Predicates is the condition, reference references a cell in on of the cardinal directions of the cell being tested.
@@ -26,8 +29,19 @@ public class Predicate {
 
     public Predicate(Predicates p, Variable variable, boolean negated) {
         this.predicateType = p;
-        this.variable = variable;
+        this.variable1 = variable;
+        this.variable2 = null;
+        this.variableCount = 1;
         this.negated = negated;
-
     }
+
+    public Predicate(Predicates p, Variable variable1, Variable variable2, boolean negated) {
+        this.predicateType = p;
+        this.variable1 = variable1;
+        this.variable2 = variable2;
+        this.variableCount = 2;
+        this.negated = negated;
+    }
+
+
 }
